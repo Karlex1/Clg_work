d18fd2bd7297b0c06d43f66fb54f36fe2f389e66
Sanchit <sanchiue@gmail.com>
2023-11-14 15:11:31 +0530

    dsl6

--- dsl6.c
+++ dsl6.c
@@ -1,23 +1,37 @@
-#include<stdio.h>
-int main(){
-    int a;
-    int b;
-    int choice;
-    do
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {1, 0, 1, 0, 1};
+    int b[5] = {0, 1, 1, 0, 0};
+
+    // and truth table
+
+    printf("Truth table of AND\n");
+    for (int i = 0; i < 5; i++)
     {
-        printf("enter a number");
-        scanf("%d", &a);
-        printf("enter second number");
-        scanf("%d", &b);
-        mul(a, b);
-        // printf("choice : y/n\n");
-        // scanf("%d", &choice);
- } while (1);
-   
-   
-}
-void mul(int a,int b){
-    int e;
-    e = a * b;
-    printf("%d", e);
+        int d[5];
+        d[i] = (a[i] & b[i]);
+        printf("%d AND %d : %d\n", a[i], b[i], d[i]);
+    }
+
+    // or truth table
+
+    printf("Truth table of OR \n");
+    for (int i = 0; i < 5; i++)
+    {
+        int d[5];
+        d[i] = (a[i] || b[i]);
+        printf("%d OR %d : %d\n", a[i], b[i], d[i]);
+    }
+
+    // NOT Truth table
+
+    printf("Truth table of NOT \n");
+    for (int i = 0; i < 5; i++)
+    {
+        int d[5];
+        d[i] = !(a[i]);
+        printf("!%d  : %d\n", a[i], d[i]);
+    }
 }
